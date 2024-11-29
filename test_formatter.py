5fe4f5081dbd61f5d529b5c2e2d2f7233dfdd5a5
tomas_agence <tomas.sanhueza@sp.agence.com.br>
2024-11-29 13:19:53 -0300

    feat: first commit

---
+++ test_formatter.py
@@ -0,0 +1,18 @@
+def calculate_sum(a, b):
+    """This function calculates sum of two numbers"""
+    x = 1
+    y = 2
+    z = 3
+    unused_variable = 10
+    return a + b
+
+
+def print_hello_world():
+    print("Hello, World!")
+    return None
+
+
+if __name__ == "__main__":
+    result = calculate_sum(10, 20)
+    print(f"Result is: {result}")
+    print_hello_world()
